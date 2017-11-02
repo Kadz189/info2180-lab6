ee84e3a63905ffe6583675dc8bf83ee5be0b2882
Kadz189 <32443312+Kadz189@users.noreply.github.com>
2017-11-02 01:42:50 -0500

    Add files via upload

--- request.php
+++ request.php
@@ -4,6 +4,38 @@
 // respond with a value
 
 $query = $_GET['q'];
+if (q == "&all=true")
+{
+    $xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+    <meanings>
+    <definition name="definition" author="Lionel">
+    a statement of the exact meaning of a word, especially in a dictionary.
+    </definition>
+    <definition name="bar" author="Matthew">
+    a place that sells alcholic beverages
+    </definition>
+    <definition name="ajax" author="Kadeem">         
+    technique which involves the use of javascript and xml
+    </definition>
+    <definition name="html" author="Barry">
+    The standard markup language for creating web pages and web applications.
+    </definition>
+     <definition name="css" author="Steven">
+    A style sheet language used for describing the presentation of a document written in a markup language.
+    </definition>
+     <definition name="javascript" author="Andrey">
+    A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+    </definition>
+    <definition name="php" author="Trent">
+    A server-side scripting language, and a powerful tool for making dynamic and interactive websites
+    </definition>
+    </meanings>';
+    header('Content-Type: text/xml');
+    $xmlOutput = new SimpleXMLElement($xmldata);
+    echo $xmlOutput->asXML();
+}
+else
+{
 $definition = [
     "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
     "bar" => "A place that sells alcholic beverages",
@@ -16,3 +48,4 @@ $definition = [
 
 print "<h3>" . strtoupper($query) . "</h3>";
 print "<p>" . $definition[$query] . "</p>";
+}
